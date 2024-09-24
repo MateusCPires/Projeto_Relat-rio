6b996407a821dddcd674089e44a3aa7bd2c63c83
Mateus <mateuscardosopires09@gmail.com>
2024-09-24 10:49:12 -0300

    feat: Adicionar formulário de login e configurá-lo

---
+++ Projeto_Relatório/Formulários/frmLogin.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Projeto_Relatório.Formulários
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtNomeUsuario = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.errErro = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errErro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(413, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome do Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(413, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 44);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(421, 174);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(365, 26);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // txtNomeUsuario
+            // 
+            this.txtNomeUsuario.Location = new System.Drawing.Point(421, 85);
+            this.txtNomeUsuario.Name = "txtNomeUsuario";
+            this.txtNomeUsuario.Size = new System.Drawing.Size(365, 26);
+            this.txtNomeUsuario.TabIndex = 3;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold);
+            this.btnOK.Location = new System.Drawing.Point(421, 206);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(92, 41);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold);
+            this.btnCancelar.Location = new System.Drawing.Point(519, 206);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(147, 41);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(395, 426);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // errErro
+            // 
+            this.errErro.ContainerControl = this;
+            // 
+            // frmLogin
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtNomeUsuario);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errErro)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtNomeUsuario;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ErrorProvider errErro;
+    }
+}
---
+++ Projeto_Relatório/Formulários/frmLogin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Relatório.Formulários
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        int Erros = 0;
+
+        private bool CaixasOK()
+        {
+            if (txtNomeUsuario.Text == "")
+            {
+                errErro.SetError(txtNomeUsuario, "Informar usuário");
+                return false;
+            }
+            else
+            {
+                errErro.SetError(txtNomeUsuario, "");
+            }
+            if (txtSenha.Text == "")
+            {
+                errErro.SetError(txtSenha, "Informar senha");
+                return false;
+            }
+            else
+            {
+                errErro.SetError(txtSenha, "");
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (CaixasOK())
+            {
+                if(txtNomeUsuario.Text!="Eu" && txtSenha != "123")
+                {
+                    MessageBox.Show("Usuário ou senha inválidos");
+                    Erros++;
+                    if (Erros == 3)
+                    {
+                        MessageBox.Show("Número máximo de tentativas gasto");
+                        this.Close();
+                    }
+                }
+            }
+            else
+            {
+                Properties.Settings.Default.NivelUsuarioLogado = 1;
+                Properties.Settings.Default.NomeUsuarioLogado = txtNomeUsuario.Text;
+                frmMenu objMenu = new frmMenu();
+                objMenu.Show();
+                this.Close();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+    }
+}
